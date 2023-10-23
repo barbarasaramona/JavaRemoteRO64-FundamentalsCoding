7d08568553f09b674698b5fccae6ad3e2661ca28
Daniel 💎 <paladedani99@gmail.com>
2023-10-23 20:13:22 +0300

    Daniel:forma geoemtrica+dreptunghi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Dreptunghi dr1=new Dreptunghi(1,2);
         System.out.println(dr1.calculperimetru());
+        System.out.println("Test");
     }
 }
