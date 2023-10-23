a24619362e8b51dde66288acd32c863c05d77110
Daniel 💎 <paladedani99@gmail.com>
2023-10-23 21:23:40 +0300

    Daniel:forma geoemtrica+dreptunghi+cerc

--- src/main/java/org/example/Dreptunghi.java
+++ src/main/java/org/example/Dreptunghi.java
@@ -1,21 +1,35 @@
 package org.example;
 
 public class Dreptunghi extends FormaGeometrica {
-   private float lungime;
-   private float latime;
+    private float lungime;
+    private float latime;
 
 
-    public Dreptunghi(){
+    public Dreptunghi() {
         System.out.println("Constructor default din dreptunghi.");
     }
-    public Dreptunghi(float lungime,float latime){
-        super(0,0);
-        this.lungime=lungime;
-        this.latime=latime;
-     //   System.out.println("Constructor cu parmaterii din dreptunghi.");
+
+    public Dreptunghi(float lungime, float latime) {
+          super(2, 2);
+        this.lungime = lungime;
+        this.latime = latime;
+        //   System.out.println("Constructor cu parmaterii din dreptunghi.");
     }
-    public float calculperimetru(){
-        float rezultat= 2*(this.lungime+this.latime);
+
+    public float calculPerimetru() {
+        float rezultat = 2 * (this.lungime + this.latime);
         return rezultat;
+    }
+
+    public float calculArie() {
+        float rezultat = this.lungime * this.latime;
+        super.arie = rezultat;
+        return rezultat;
+    }
+
+    @Override
+    public float calculPerimetruFormaGeometrica() {
+        float rezultat = 2 * (this.lungime + this.latime);
+        return super.calculPerimetruFormaGeometrica() + rezultat;
     }
 }
--- src/main/java/org/example/FormaGeometrica.java
+++ src/main/java/org/example/FormaGeometrica.java
@@ -3,13 +3,18 @@ package org.example;
 public class FormaGeometrica {
     protected float arie;
     protected float perimetru;
-    public FormaGeometrica(float arie, float perimetru){
-        this.arie=arie;
-        this.perimetru=perimetru;
-    //    System.out.println("Constructor cu paramtru din clasa de baza.");
-    }
-    public FormaGeometrica(){
-        System.out.println("Constructor default din clasa de baza.");
+
+    public FormaGeometrica(float arie, float perimetru) {
+        this.arie = arie;
+        this.perimetru = perimetru;
+        //    System.out.println("Constructor cu paramtru din clasa de baza.");
     }
 
+    public FormaGeometrica() {
+      //  System.out.println("Constructor default din clasa de baza.");
+    }
+    public float calculPerimetruFormaGeometrica(){
+
+        return perimetru;
+    }
 }
