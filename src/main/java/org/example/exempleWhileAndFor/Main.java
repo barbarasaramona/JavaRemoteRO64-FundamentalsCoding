730f80222d43da1541077e7fb4d54ffa28318c7b
barbarasaramona <barbarasaramona@yahoo.com>
2023-10-26 12:49:29 +0300

    DO WHILE sume

--- src/main/java/org/example/exempleWhileAndFor/Main.java
+++ src/main/java/org/example/exempleWhileAndFor/Main.java
@@ -11,16 +11,23 @@ public class Main {
         System.out.println("Suma numerelor de la 1 la 10 calculata cu for: " + sumaCalculataCuFor);
 
         //Varianta cu WHILE
-        int j = 1;
+        int j = 0;
         int sumaCalculataCuWhile = 0;
         while (j <= 10) {
             sumaCalculataCuWhile = sumaCalculataCuWhile + j;
-            j += 1;
+            j++;
         }
         System.out.println("Suma numerelor de la 1 la 10 calculata cu while: " + sumaCalculataCuWhile);
 
         //TODO
         //Varianta DO WHILE
+        int cursor = 0;
+        int sumaCalculataCuDoWilde = 0;
+        do {
+            sumaCalculataCuDoWilde = sumaCalculataCuDoWilde + cursor;
+            cursor++;
+        } while (cursor <= 10);
+        System.out.println("Suma nr de la 1 la 10 cu DO WHILE: " + sumaCalculataCuDoWilde);
 
         //Sa se calculeze suma numerelor pare de la 0 la 10
         //Varianta cu FOR
@@ -43,6 +50,13 @@ public class Main {
 
         //TODO
         //Varianta DO WHILE
+        int cursor1 = 0;
+        int sumaNrPareCalculataCuDoWhile = 0;
+        do {
+            sumaNrPareCalculataCuDoWhile = sumaNrPareCalculataCuDoWhile + cursor1;
+            cursor1 += 2;
+        } while (cursor1 <= 10);
+        System.out.println("Suma nr pare cu DO WHILE: " + sumaNrPareCalculataCuDoWhile);
 
         //Sa se calculeze suma numerelor impare de la 0 la 10
         //Varianta cu FOR
@@ -65,6 +79,13 @@ public class Main {
 
         //TODO
         //Varianta DO WHILE
+        int cursor2 = 1;
+        int sumaNrImpareCalculataCuDoWhile = 0;
+        do {
+            sumaNrImpareCalculataCuDoWhile = sumaNrImpareCalculataCuDoWhile + cursor2;
+            cursor2 += 2;
+        } while (cursor2 <= 10);
+        System.out.println("Suma nr impare cu DO WHILE: " + sumaNrImpareCalculataCuDoWhile);
 
 
     }
